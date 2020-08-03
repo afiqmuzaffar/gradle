83dbf74e97fb39abc8e908e246d76fd780b7c382
Johannes Bühler <johannes@gradle.com>
2020-08-03 23:30:37 +0200

    ensure test-retry-plugin usages are considered carefully when changes are applied to the internal api
    Signed-off-by: Johannes Bühler <johannes@gradle.com>

--- subprojects/core/src/main/java/org/gradle/api/internal/project/ProjectInternal.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/project/ProjectInternal.java
@@ -48,7 +48,7 @@ import org.gradle.util.Path;
 
 import javax.annotation.Nullable;
 
-@UsedByScanPlugin
+@UsedByScanPlugin //and test-retry-plugin
 public interface ProjectInternal extends Project, ProjectIdentifier, HasScriptServices, DomainObjectContext, DependencyMetaDataProvider, ModelRegistryScope, PluginAwareInternal {
 
     // These constants are defined here and not with the rest of their kind in HelpTasksPlugin because they are referenced
@@ -88,7 +88,7 @@ public interface ProjectInternal extends Project, ProjectIdentifier, HasScriptSe
     DynamicObject getInheritedScope();
 
     @Override
-    @UsedByScanPlugin("test-distribution")
+    @UsedByScanPlugin("test-distribution, test-retry")
     GradleInternal getGradle();
 
     ProjectEvaluationListener getProjectEvaluationBroadcaster();
@@ -99,7 +99,7 @@ public interface ProjectInternal extends Project, ProjectIdentifier, HasScriptSe
 
     FileResolver getFileResolver();
 
-    @UsedByScanPlugin
+    @UsedByScanPlugin //and test-retry-plugin
     ServiceRegistry getServices();
 
     ServiceRegistryFactory getServiceRegistryFactory();
